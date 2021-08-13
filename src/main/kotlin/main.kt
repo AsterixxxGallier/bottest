932956997688f7d90ff731817dc7ee7a19dd5a10
AsterixxxGallier <asterixxxgallier@aol.com>
2021-08-13 16:28:34 +0200

    Added token secret access

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,8 +5,7 @@ import com.gitlab.kordlib.core.on
 import kotlinx.coroutines.delay
 
 suspend fun main() {
-	//TODO secure the token!!!
-	val client = Kord("ODc1NzQ1Mjg4MDU1MjkxOTM0.YRZ_Ww.a8Yn2igNVW_AXkrTiZ8h7_fISxM")
+	val client = Kord(System.getenv("BOT_TOKEN"))
 	val pingPong = ReactionEmoji.Unicode("\uD83C\uDFD3")
 
 	client.on<MessageCreateEvent> {
