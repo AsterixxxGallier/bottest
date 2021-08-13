4afe5bfdc46cdb3fbf6aa2fdec05cf94dcfcb295
AsterixxxGallier <asterixxxgallier@aol.com>
2021-08-13 16:15:59 +0200

    Added Kord dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,9 +10,13 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    jcenter()
+    maven(url = "https://dl.bintray.com/kordlib/Kord")
 }
 
-
+dependencies {
+    implementation("com.gitlab.kordlib.kord:kord-core:0.6.3")
+}
 
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "13"
