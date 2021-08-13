85a672ee1fcc76a3f777863a2658a2d6f99f8dc4
AsterixxxGallier <asterixxxgallier@aol.com>
2021-08-13 16:23:17 +0200

    Added Kord example code

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,25 @@
-fun main(args: Array<String>) {
-	println("Hello World!")
+import com.gitlab.kordlib.core.Kord
+import com.gitlab.kordlib.core.entity.ReactionEmoji
+import com.gitlab.kordlib.core.event.message.MessageCreateEvent
+import com.gitlab.kordlib.core.on
+import kotlinx.coroutines.delay
+
+suspend fun main() {
+	//TODO secure the token!!!
+	val client = Kord("HEyMUsUGoWA38Kf-2vw2NonoYELNntN5")
+	val pingPong = ReactionEmoji.Unicode("\uD83C\uDFD3")
+
+	client.on<MessageCreateEvent> {
+		if(message.content != "!ping") return@on
+
+		val response = message.channel.createMessage("Pong!")
+		response.addReaction(pingPong)
+
+		delay(5000)
+		message.delete()
+		response.delete()
+	}
+
+	client.login()
+
 }
